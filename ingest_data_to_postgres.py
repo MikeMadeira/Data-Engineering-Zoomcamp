7a8109317c5cf9271c1ef8ea8973ecacf1a0845c
Michael Sousa Madeira <mikemadeira_aux@MikeMadeira.mikemadeira>
2024-01-19 07:32:17 +0000

    Finished homework 1 on the Docker part | Added a way to input a file to be ingested apart from the URL | Ingested another file to postgres database and queried the database

--- ingest_data_to_postgres.py
+++ ingest_data_to_postgres.py
@@ -18,13 +18,22 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
+    file = params.file
     
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
+    if (file is not None) and (url is None):
+        csv_name = file
+    elif ((file is None) and (url is not None)) or ((file is None) and (url is None)):
+        if url.endswith('.csv.gz'):
+            csv_name = 'output.csv.gz'
+        else:
+            csv_name = 'output.csv'
     else:
-        csv_name = 'output.csv'
+        print('No file or URL were given as input to ingest.')
+        return None
+
+
 
     os.system(f"wget {url} -O {csv_name}")
 
@@ -70,7 +79,8 @@ if __name__ == '__main__':
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--url', required=False, help='url of the csv file, which has priority over file')
+    parser.add_argument('--file', required=False, help='csv file to be ingested')
 
     args = parser.parse_args()
 
